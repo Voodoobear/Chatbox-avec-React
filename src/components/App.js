3dff5aae95de663969a6797a2d674036cc81df22
Voodoobear <s.chalain@gmail.com>
2018-09-13 14:58:07 +0200

    ajout du compteur de décrémentation, association des pseudos par messages, gestion des states

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,29 @@ import Formulaire from './Formulaire';
 import Messages from './Messages';
 
 class App extends React.Component {
+
+    state = {
+        messages: {}
+    }
+
+    addMessage = message => {
+        // Copy the state
+        const messages = {...this.state.messages};
+        // Adding messages with timestamps
+        const timestamp = Date.now();
+        messages[`message-${timestamp}`] = message;
+        // Update the state
+        this.setState({ messages });
+    }; 
+
     render() {
         return (
            <div className="box">
                <div>
                    <div className="messages">
-                   <Messages pseudo="Seb" />
+                   <Messages pseudo={this.props.params.pseudo} />
                    </div>
-                   <Formulaire />
+                   <Formulaire addMessage={this.addMessage} pseudo={this.props.params.pseudo} length="140" />
                </div>
            </div>
         )
--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,16 +1,35 @@
 import React from 'react';
 
 class Connexion extends React.Component {
+    
+    goToChat = event => {
+        event.preventDefault();
+        //Recuperer le pseudo
+        const pseudo = this.pseudoInput.value;
+        //Changer l'URL
+        this.context.router.transitionTo(`/pseudo/${pseudo}`)
+    };
+    
     render() {
         return (
-           <div className="connexionBox">
+           <div className="connexionBox" onSubmit={e => this.goToChat(e)}>
                <form className="connexion">
-                   <input type="text" placeholder="Pseudo" required/>
+                   <input 
+                    type="text" 
+                    placeholder="Pseudo" 
+                    required 
+                    ref={input => {this.pseudoInput = input}}
+                   />
                    <button type="submit">Go</button>
                </form>
            </div> 
         )
     }
+
+    static contextTypes = {
+        router: React.PropTypes.object
+    }
+
 }
 
 export default Connexion;
